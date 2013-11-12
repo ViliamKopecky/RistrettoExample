bd2dc451a94d8188ed2fd19b20c8f6f5fff8c2a4
ViliamKopecky <vk@viliamkopecky.cz>
2013-11-12 22:18:45 +0100

    Simplyfied example; Updated to Ristretto 1.0.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,42 @@
 module.exports = function (grunt) {
 
-  var port = 2013;
-
-  var publish_dir = 'publish';
-  var www_dir = 'www';
-  var latte_dir = 'www';
-  var model_dir = 'www/model';
-
-  var less_files = {};
-  less_files['www/css/screen.css'] = 'www/less/screen.less';
-  //less_files['www/css/print.css'] = 'www/less/print.less';
+  var config_path = __dirname + '/ristretto.json';
+  var config = grunt.file.readJSON(config_path);
+  config.config_path = config_path;
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean: {
-      publish: [ publish_dir ]
-    },
-    copy: {
-      publish: {
-        files: [
-          { expand: true, cwd: www_dir, src: ['**', '!**.latte'], dest: publish_dir }
-        ]
-      }
-    },
+
+
     ristretto: {
       options: {
-        model_dir: model_dir,
-        latte_dir: latte_dir,
-        www_dir: www_dir,
-        publish_dir: publish_dir,
-        port: port
+        config: config
       }
     },
+
+
     esteWatch: {
       options: {
         livereload: { enabled: false },
-        dirs: [ www_dir+'/**' ]
+        dirs: [ config.www_dir+'/**' ]
       },
-      less: function(filepath) {
-        return ['less'];
-      },
-      css: function(filepath) {
+      "css": function(filepath) {
         return ['ristretto:stylesheets'];
       },
       "*": function(filepath) {
         return ['ristretto:pages'];
       }
     },
-    less: {
-      production: {
-        options: { yuicompress: true },
-        files: less_files
-      }
-    }
-  });
-  
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-ristretto');
-  grunt.loadNpmTasks('grunt-este-watch');
 
-  grunt.registerTask('default', ['ristretto:server', 'less', 'ristretto:pages', 'esteWatch']);
-  grunt.registerTask('publish', ['less', 'ristretto:publish', 'copy:publish']);
+    release: {}
+
+  });
+
+  grunt.loadNpmTasks('grunt-este-watch');
+  grunt.loadNpmTasks('grunt-ristretto');
+
+  grunt.loadNpmTasks('grunt-release');
+
+  grunt.registerTask('dev', ['ristretto:startup', 'esteWatch']);
+  grunt.registerTask('build', ['ristretto:publish']);
 };
